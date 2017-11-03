4c3961ff82022764b8bf03715e438ff7a07a3982
Jingyun Yang <jingyuny@usc.edu>
2017-11-03 14:47:03 -0700

    Fixed render bugs and added randomization and fading.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+var PRINT_CONTROL = false;
+var PRINT_CAM = false;
+
 var scene, camera, renderer;
 var controls;
 var spheres = [];
@@ -34,6 +37,7 @@ function createScene() {
 
 	renderer = new THREE.WebGLRenderer( { alpha: true } );
 	renderer.setSize(WIDTH, HEIGHT);
+	renderer.setClearColor( 0x000000, 0 );
 
 	camera = new THREE.PerspectiveCamera(
 		VIEW_ANGLE,
@@ -47,21 +51,25 @@ function createScene() {
 	container.appendChild(renderer.domElement);
 
 	var parameters = {};
-	parameters.z = -200;
+	parameters.z = -80;
 
 	// ADD A BOX
 
 	for(var x = minX; x <= maxX; x += pad) {
 		for(var y = minY; y <= maxY; y+= pad) {
-			var sphere = addSphere(1, {x: x, y: y, z: -270}, 0xff0000);
+			var randRadius = Math.sqrt(Math.random());
+			var randHue = Math.floor(Math.random() * 360) + 1;
+			var sphere = addSphere(randRadius, {x: x, y: y, z: -270}, "hsla(" + randHue + ", 100%, 50%, 1)");
 			spheres.push(sphere);
 		}
 	}
 
 	// DEBUGGING
 
-	gui = new dat.GUI();
-	gui.add(parameters, 'z', -500, 100).onChange(updatePlacement);
+	if(PRINT_CONTROL) {
+		gui = new dat.GUI();
+		gui.add(parameters, 'z', -500, 100).onChange(updatePlacement);
+	}
 
 	// LIGHTS
 
@@ -126,7 +134,7 @@ function addBox(sidelength, position, color) {
 function addSphere(radius, position, color) {
 	const SEGMENTS = 5;
 	const RINGS = 5;
-	var material = new THREE.MeshBasicMaterial( {color: color, transparent: true, opacity: 0.5} );
+	var material = new THREE.MeshBasicMaterial( {color: color, transparent: true, opacity: 0.0} );
 	var sphere = new THREE.Mesh( 
 		new THREE.SphereGeometry( radius, SEGMENTS, RINGS ), 
 		material 
@@ -138,8 +146,17 @@ function addSphere(radius, position, color) {
 	return sphere;
 }
 
-function setSphereColor(index, opacity) {
-	spheres[index].material.opacity = opacity;
+function setSphereOpacity(index, opacity) {
+	if(spheres[index].material.opacity < opacity)
+		spheres[index].material.opacity = opacity;
+}
+
+function increaseSphereOpacity(index, amount) {
+	spheres[index].material.opacity = spheres[index].material.opacity + amount;
+}
+
+function decreaseSphereOpacity(index, amount) {
+	spheres[index].material.opacity = spheres[index].material.opacity - amount;
 }
 
 function detectHand() {
@@ -175,7 +192,9 @@ function detectHand() {
         // Draw video overlay:
 		canvas.width = ~~(100 * video.videoWidth / video.videoHeight);
 		canvas.height = 100;
-		context.drawImage(video, 0, 0, canvas.clientWidth, canvas.clientHeight);
+		if(PRINT_CAM) {
+			context.drawImage(video, 0, 0, canvas.clientWidth, canvas.clientHeight);
+		}
 		
 		if (video.readyState === video.HAVE_ENOUGH_DATA && video.videoWidth > 0) {
 		
@@ -209,20 +228,23 @@ function detectHand() {
 				// Rescale coordinates from detector to three.js drawing coordinate space:
 				var displayDrawWidth = cols * 2 * pad;
 				var displayDrawHeight = rows * 2 * pad;
-				displayCoord[0] = Math.floor(coord[0] * displayDrawWidth / detector.canvas.width - displayDrawWidth / 2);
-				displayCoord[1] = Math.floor(coord[1] * displayDrawHeight / detector.canvas.height - displayDrawHeight / 2);
-				displayCoord[2] = Math.ceil((coord[0] + coord[2]) * displayDrawWidth / detector.canvas.width - displayDrawWidth / 2);
-				displayCoord[3] = Math.ceil((coord[1] + coord[3]) * displayDrawHeight / detector.canvas.height - displayDrawHeight / 2);
+				displayCoord[0] = Math.floor(coord[0] * displayDrawWidth / drawWidth - displayDrawWidth / 2);
+				displayCoord[1] = Math.floor(coord[1] * displayDrawHeight / drawHeight - displayDrawHeight / 2);
+				displayCoord[2] = Math.ceil((coord[0] + coord[2]) * displayDrawWidth / drawWidth - displayDrawWidth / 2);
+				displayCoord[3] = Math.ceil((coord[1] + coord[3]) * displayDrawHeight / drawHeight - displayDrawHeight / 2);
 
 				// log canvas sizes
-				document.getElementById("log").innerHTML  = "Min & max of x & y: " + minX + "," + maxX + "," + minY + "," + maxY;
+				document.getElementById("log").innerHTML  = "The Min & max of x & y: " + minX + "," + maxX + "," + minY + "," + maxY;
 				document.getElementById("log").innerHTML += "<br/>Original: " + detector.canvas.width + " " + detector.canvas.height;
 				document.getElementById("log").innerHTML += "<br/>Video: " + drawWidth + " " + drawHeight;
 				document.getElementById("log").innerHTML += "<br/>Display: " + displayDrawWidth + " " + displayDrawHeight;
 
 				// draw on three.js if recognition is stable
 				if (fist_pos_old) {
-					drawRect(displayCoord);
+					drawInRect(displayCoord);
+				}
+				else {
+					drawInRect([1000,1000,1000,1000]);
 				}
 				
 				// log coordinate positions
@@ -260,11 +282,13 @@ function detectHand() {
 				}
 			
 				// Draw coordinates on video overlay:
-				context.beginPath();
-				context.lineWidth = '2';
-				context.fillStyle = fist_pos_old ? 'rgba(0, 255, 255, 0.5)' : 'rgba(255, 0, 0, 0.5)';
-				context.fillRect(coord[0], coord[1], coord[2], coord[3]);
-				context.stroke();
+				if(PRINT_CAM) {
+					context.beginPath();
+					context.lineWidth = '2';
+					context.fillStyle = fist_pos_old ? 'rgba(0, 255, 255, 0.5)' : 'rgba(255, 0, 0, 0.5)';
+					context.fillRect(coord[0], coord[1], coord[2], coord[3]);
+					context.stroke();
+				}
 			} else fist_pos_old = null;
 		}
 	}
@@ -274,16 +298,37 @@ function inRange(num, low, high) {
 	return (num >= low) && (num <= high);
 }
 
-function drawRect(coords) {
+function inCircle(coords, center, radius) {
+	return Math.pow(coords[1] - center[1], 2.0) + Math.pow(coords[0] - center[0], 2.0) <= radius * radius;
+}
+
+function getIntensityBasedOnDistance(coords, center, radius) {
+	var dist = Math.sqrt(Math.pow(coords[1] - center[1], 2.0) + Math.pow(coords[0] - center[0], 2.0));
+	if(dist >= radius) return 0;
+	else {
+		var ratio = dist / radius;
+		return Math.cos(ratio * Math.PI / 2);
+	}
+}
+
+function drawInRect(coords) {
+	var center = [(coords[0] + coords[2]) / 2, (coords[1] + coords[3]) / 2];
+	var radius = ((coords[2] - coords[0]) / 2 + (coords[3] - coords[1]) / 2) / 2;
+	
 	var draw = coords.length >= 4;
 	index = 0;
 	for(var x = maxX; x >= minX; x -= pad) {
 		for(var y = maxY; y >= minY; y -= pad) {
-			if(draw && inRange(x, coords[0], coords[2]) && inRange(y, coords[1], coords[3])) {
-				setSphereColor(index, 1.0);
+			if(draw) {
+				if(inCircle([x,y], center, radius)) {
+					setSphereOpacity(index, getIntensityBasedOnDistance([x,y], center, radius));
+				}
+				else {
+					decreaseSphereOpacity(index, 0.1);
+				}
 			}
 			else {
-				setSphereColor(index, 0.5);
+				decreaseSphereOpacity(index, 0.1);
 			}
 			index++;	
 		}
